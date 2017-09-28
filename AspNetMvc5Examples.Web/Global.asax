a8fc02e9f45bc1d10e290ee542070d49a85823b2
Lukáš Kmoch <lukas.kmoch@designeo.cz>
2017-09-28 11:19:28 +0200

    Add solution with web project

---
+++ AspNetMvc5Examples.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetMvc5Examples.Web.MvcApplication" Language="C#" %>
---
+++ AspNetMvc5Examples.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetMvc5Examples.Web.Startup))]
+namespace AspNetMvc5Examples.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
