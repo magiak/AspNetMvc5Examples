2b669d50d7923f1f3d6c395dfae4b197c5f926e8
Lukáš Kmoch <lukas.kmoch@designeo.cz>
2017-10-02 22:49:51 +0200

    Add area for RequestLifeCycle code

---
+++ AspNetMvc5Examples.Web/Areas/RequestLifeCycle/RequestLifeCycleAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AspNetMvc5Examples.Web.Areas.RequestLifeCycle
+{
+    public class RequestLifeCycleAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "RequestLifeCycle";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "RequestLifeCycle_default",
+                "RequestLifeCycle/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
