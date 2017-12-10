91734f1dd213e183f2ec7885f78826ef9fe04844
Lukáš Kmoch <lukas.kmoch@designeo.cz>
2017-12-10 20:25:46 +0100

    Add razor syntax examples

---
+++ AspNetMvc5Examples.Web/Controllers/RazorSyntaxController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvc5Examples.Web.Controllers
+{
+    public class RazorSyntaxController : Controller
+    {
+        // GET: RazorSyntax
+        public ActionResult Examples()
+        {
+            return this.View();
+        }
+    }
+}
