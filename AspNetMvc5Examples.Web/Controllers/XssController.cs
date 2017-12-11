4cb034a518455947aa8d3fba0213d96bf07a37d6
Lukáš Kmoch <lukas.kmoch@designeo.cz>
2017-12-11 22:59:57 +0100

    Add example of XSS attack

---
+++ AspNetMvc5Examples.Web/Controllers/XssController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvc5Examples.Web.Controllers
+{
+    using Models;
+
+    public class XssController : Controller
+    {
+        // GET: Xss
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new XssViewModel();
+            return this.View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateInput(true)]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(XssViewModel viewModel)
+        {
+            return this.View(viewModel);
+        }
+    }
+}
---
+++ AspNetMvc5Examples.Web/Models/XssViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AspNetMvc5Examples.Web.Models
+{
+    public class XssViewModel
+    {
+        public string Text { get; set; }
+        
+    }
+}
