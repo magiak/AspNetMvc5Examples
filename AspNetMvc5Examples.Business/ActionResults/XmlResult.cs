3eb3d228e8a59056305b4d54942d6bdae5487595
Lukáš Kmoch <lukas.kmoch@designeo.cz>
2018-03-15 17:51:56 +0100

    Add XmlActionResult

---
+++ AspNetMvc5Examples.Business/ActionResults/XmlResult.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace AspNetMvc5Examples.Business.ActionResults
+{
+    public class XmlResult : ActionResult
+    {
+        public XmlResult(object obj)
+        {
+            this.objectToSerialize = obj;
+        }
+
+        public object objectToSerialize { get; set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            context.HttpContext.Response.Clear();
+            var xs = new System.Xml.Serialization.XmlSerializer(this.objectToSerialize.GetType());
+            context.HttpContext.Response.ContentType = "text/xml";
+            xs.Serialize(context.HttpContext.Response.Output, this.objectToSerialize);
+        }
+    }
+}
