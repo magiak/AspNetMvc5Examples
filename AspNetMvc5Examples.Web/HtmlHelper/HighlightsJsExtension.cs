1f77c8d21a812de01b95dfd71bbda0bc7f9d59db
Lukáš Kmoch <lukas.kmoch@designeo.cz>
2018-03-17 20:14:18 +0100

    Refactor model binding

---
+++ AspNetMvc5Examples.Web/HtmlHelper/HighlightsJsExtension.cs
@@ -0,0 +1,17 @@
+﻿namespace AspNetMvc5Examples.Web.HtmlHelper
+{
+    using System.Web;
+    using System.Web.Mvc;
+
+    public static class HighlightsJsExtension
+    {
+        public static MvcHtmlString CodeSection(this HtmlHelper html, string code)
+        {
+            code = HttpUtility.HtmlDecode(code);
+            code = html.Encode(code);
+            var result = $@"<div class=""hljs-wrapper""><pre><code class=""html"">{code}</code></pre></div>";
+
+            return MvcHtmlString.Create(result);
+        }
+    }
+}
