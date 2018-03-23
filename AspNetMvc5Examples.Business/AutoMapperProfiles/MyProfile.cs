a6388712b1790584fd6e04d9515504fd4322795e
Lukáš Kmoch <lukas.kmoch@designeo.cz>
2018-03-23 10:56:20 +0100

    Add AutoMapper and use it in MoviesController

---
+++ AspNetMvc5Examples.Business/AutoMapperProfiles/MyProfile.cs
@@ -0,0 +1,15 @@
+﻿using AspNetMvc5Examples.Entities.Models;
+using AspNetMvc5Examples.Entities.ViewModels;
+using AutoMapper;
+
+namespace AspNetMvc5Examples.Business.AutoMapperProfiles
+{
+    public class MyProfile : Profile
+    {
+        public MyProfile()
+        {
+            // Movies
+            this.CreateMap<Movie, MovieViewModel>().ReverseMap();
+        }
+    }
+}
--- AspNetMvc5Examples.Web/Controllers/MoviesController.cs
+++ AspNetMvc5Examples.Web/Controllers/MoviesController.cs
@@ -1,13 +1,36 @@
-﻿using System;
+﻿using AspNetMvc5Examples.Entities.Models;
+using AspNetMvc5Examples.Entities.ViewModels;
+using AutoMapper;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace AspNetMvc5Examples.Web.Controllers
 {
     public class MoviesController : Controller
     {
+        public List<Movie> DatabaseMovies { get; set; } = new List<Movie>();
+        public MoviesController()
+        {
+            this.DatabaseMovies.AddRange(new[]
+            {
+                new Movie(){ Id = 1, Title = "Homer" },
+                new Movie(){ Id = 1, Title = "Pelisky" }
+            });
+        }
+
+        public ActionResult AutoMapperTest()
+        {
+            var movies = this.DatabaseMovies.Select(x => Mapper.Map<Movie, MovieViewModel>(x));
+            return this.Json(movies, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Index()
+        {
+            var movies = new { };
+            return this.Json(movies);
+        }
+
         // GET: Movies
         public ActionResult Released(int year, int month)
         {
