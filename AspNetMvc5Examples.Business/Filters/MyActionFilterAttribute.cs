66eed406debefea76fccd6dbdf822102e2055131
Lukáš Kmoch <lukas.kmoch@designeo.cz>
2018-04-17 23:24:28 +0200

    Add custom cookie authentication middleware

---
+++ AspNetMvc5Examples.Business/Filters/MyActionFilterAttribute.cs
@@ -0,0 +1,44 @@
+﻿namespace AspNetMvc5Examples.Business.Filters
+{
+    using System.Diagnostics;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    public class MyActionFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            //Debugger.Break();
+
+            // This is to late
+            //filterContext.RequestContext.RouteData.Values.Remove("action");
+            //filterContext.RequestContext.RouteData.Values.Add("action", "Test2");
+
+
+            // filterContext.Result is null;
+
+            //filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+            //{
+            //    controller = "Filter",
+            //    action = "Test2"
+            //}));
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            // filter.Result is ContentResult
+
+            //Debugger.Break();
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            //Debugger.Break();
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            //Debugger.Break();
+        }
+    }
+}
---
+++ AspNetMvc5Examples.Business/Middleware/MyOwinMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using AspNetMvc5Examples.Entities.DbContexts;
+using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetMvc5Examples.Business.Middleware
+{
+    public class MyOwinMiddleware : OwinMiddleware
+    {
+        public static string AuthenticationKey = "MyAspNet.ApplicationCookie";
+        private static string LoginPage = "/myaccount/login";
+        private static string RegisterPage = "/myaccount/register";
+        private readonly OwinMiddleware next;
+
+        public MyOwinMiddleware(OwinMiddleware next) : base(next)
+        {
+            this.next = next;
+        }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            var path = context.Request.Environment["owin.RequestPath"].ToString().ToLower();
+            var cookie = context.Request.Cookies[AuthenticationKey];
+            if (cookie == null && path != LoginPage && path != RegisterPage)
+            {
+                context.Response.Redirect(LoginPage);
+                return;
+            }
+
+            if (path == LoginPage || path == RegisterPage)
+            {
+                await this.next.Invoke(context);
+                return;
+            }
+
+            // Get UserName and password from cookie
+            var userName = cookie.Split(':')[0];
+            var password = cookie.Split(':')[1];
+
+            // KEY: AspNet.Identity.Owin:AspNetMvc5Examples.Entities.DbContexts.ApplicationDbContext, AspNetMvc5Examples.Entities, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null, AspNetMvc5Examples.Entities.DbContexts.ApplicationDbContext]}
+            var key = $"AspNet.Identity.Owin:{typeof(ApplicationDbContext).AssemblyQualifiedName}";
+            var dbContext = context.Get<ApplicationDbContext>(key);
+
+            var user = dbContext.MyAspNetUsers.FirstOrDefault(x => x.UserName == userName && x.Password == password);
+            if(user == null)
+            {
+                context.Response.Redirect(LoginPage);
+                return;
+            }
+
+            await this.next.Invoke(context);
+        }
+    }
+}
